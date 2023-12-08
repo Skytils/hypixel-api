5089b17bdd19de664f1a14eeadb4b5cab2c88cf6
sychic <47618543+Sychic@users.noreply.github.com>
2023-12-08 11:01:52 -0500

    feat: skyblock profile

---
+++ types/build.gradle.kts
@@ -0,0 +1,16 @@
+plugins {
+    kotlin("jvm") version "1.9.20"
+    kotlin("plugin.serialization") version "1.9.20"
+}
+
+group = "gg.skytils.hypixel.types"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    compileOnly("org.jetbrains.kotlinx:kotlinx-serialization-core:1.4.1")
+    compileOnly("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.1")
+}
---
+++ types/src/main/kotlin/gg/skytils/hypixel/types/skyblock/profile.kt
@@ -0,0 +1,63 @@
+package gg.skytils.hypixel.types.skyblock
+
+import kotlinx.serialization.Contextual
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+import java.util.*
+
+@Serializable
+class Profile(
+    @Contextual
+    @SerialName("profile_id")
+    val id: UUID,
+    @SerialName("cute_name")
+    val name: String,
+    val selected: Boolean,
+    val community_upgrades: UpgradeData = UpgradeData(null)
+)
+
+@Serializable
+class UpgradeData(
+    val currently_upgrading: UpgradeInProgress?,
+    val upgrade_states: List<ProfileUpgrade> = emptyList()
+)
+
+@Serializable
+class UpgradeInProgress(
+    @SerialName("upgrade")
+    val name: String,
+    val new_tier: Int,
+    @SerialName("start_ms")
+    val started: Long,
+    @SerialName("who_started")
+    val started_by: String
+)
+
+@Serializable
+class ProfileUpgrade(
+    @SerialName("upgrade")
+    val name: String,
+    val tier: Int,
+    @SerialName("started_ms")
+    val started: Long,
+    val started_by: String,
+    @SerialName("claimed_ms")
+    val claimed: Long,
+    val claimed_by: String,
+    val fasttracked: Boolean
+)
+
+@Serializable
+class BankData(
+    val balance: Double,
+    val transactions: List<BankTransaction>
+)
+
+@Serializable
+class BankTransaction(
+    val amount: Int,
+    val timestamp: Long,
+    val action: String,
+    @SerialName("initiator_name")
+    val initiator: String // for some reason this is the color formatted name, no clue why
+)
