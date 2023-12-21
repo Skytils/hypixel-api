60954c7c34a23a8b3093d9f76725faa77689cb07
sychic <47618543+Sychic@users.noreply.github.com>
2023-12-21 17:30:16 -0500

    feat: basic hypixel player implementation

---
+++ types/src/main/kotlin/gg/skytils/hypixel/types/player/player.kt
@@ -0,0 +1,43 @@
+/*
+ * Skytils - Hypixel Skyblock Quality of Life Mod
+ * Copyright (C) 2020-2023 Skytils
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package gg.skytils.hypixel.types.player
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Player(
+    @SerialName("achievementsOneTime")
+    val achievements_one_time: List<String> = emptyList(),
+    val achievements: Map<String, Double> = emptyMap(),
+    @SerialName("socialMedia")
+    val social_media: SocialMediaData,
+    internal val newPackageRank: String = "NONE",
+    internal val monthlyPackageRank: String = "NONE",
+    @SerialName("rankPlusColor")
+    val plus_color: String
+) {
+    val rank = if (newPackageRank == "MVP_PLUS" && monthlyPackageRank == "SUPERSTAR") "MVP_PLUS_PLUS" else newPackageRank
+}
+
+@Serializable
+data class SocialMediaData(
+    val links: Map<String, String> = emptyMap(),
+    val prompt: Boolean
+)
