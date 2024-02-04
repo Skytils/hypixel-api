90896baefafd958f6ce31badffac759d02d66c9b
My-Name-Is-Jeff <37018278+My-Name-Is-Jeff@users.noreply.github.com>
2024-02-03 20:22:15 -0500

    fix: SlayerBossData doesn't get deserialized

--- types/src/main/kotlin/gg/skytils/hypixel/types/skyblock/slayer.kt
+++ types/src/main/kotlin/gg/skytils/hypixel/types/skyblock/slayer.kt
@@ -18,16 +18,16 @@
 
 package gg.skytils.hypixel.types.skyblock
 
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.InternalSerializationApi
 import kotlinx.serialization.KSerializer
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.builtins.MapSerializer
 import kotlinx.serialization.builtins.serializer
+import kotlinx.serialization.descriptors.*
 import kotlinx.serialization.encoding.Decoder
 import kotlinx.serialization.encoding.Encoder
-import kotlinx.serialization.json.JsonDecoder
-import kotlinx.serialization.json.double
-import kotlinx.serialization.json.jsonObject
-import kotlinx.serialization.json.jsonPrimitive
+import kotlinx.serialization.json.*
 
 @Serializable
 data class SlayerData(
@@ -50,24 +50,36 @@ data class SlayerKill(
     val timestamp: Long
 )
 
-@Serializable
+@Serializable(with = SlayerBossDataSerializer::class)
 data class SlayerBossData(
     val claimed_levels: Map<String, Boolean> = emptyMap(),
-    @Serializable(with = BossKillsSerializer::class)
-    val boss_kills: Map<String, Double> = emptyMap(),
+    val boss_kills: Map<String, Long> = emptyMap(),
     val xp: Double = 0.0
 )
 
-object BossKillsSerializer : KSerializer<Map<String, Double>> {
-    override val descriptor = MapSerializer(String.serializer(), Double.serializer()).descriptor
+object SlayerBossDataSerializer : KSerializer<SlayerBossData> {
+    override val descriptor = buildClassSerialDescriptor(SlayerBossData::class.qualifiedName ?: "SlayerBossData") {
+        element<Map<String, Boolean>>("claimed_levels")
+        element<Map<String, Long>>("boss_kills")
+        element<Double>("xp")
+    }
 
-    override fun deserialize(decoder: Decoder): Map<String, Double> =
-        (decoder as? JsonDecoder)?.decodeJsonElement()?.jsonObject?.entries?.filter { (key, _) ->
-            key.startsWith("boss_kills_tier_")
-        }?.associate { (key, value) ->
-            key to value.jsonPrimitive.double
-        } ?: error("Only json supported")
+    override fun deserialize(decoder: Decoder): SlayerBossData {
+        val json = (decoder as? JsonDecoder ?: error("Only json supported")).decodeJsonElement().jsonObject
 
-    override fun serialize(encoder: Encoder, value: Map<String, Double>) =
+        return SlayerBossData(
+            claimed_levels = json["claimed_levels"]?.jsonObject?.entries?.associate { (key, value) ->
+                key to value.jsonPrimitive.boolean
+            } ?: emptyMap(),
+            boss_kills = json.entries.filter { (key, _) ->
+                key.startsWith("boss_kills_tier_")
+            }.associate { (key, value) ->
+                key to value.jsonPrimitive.long
+            },
+            xp = json["xp"]?.jsonPrimitive?.double ?: 0.0
+        )
+    }
+
+    override fun serialize(encoder: Encoder, value: SlayerBossData) =
         TODO("Not yet implemented")
 }
